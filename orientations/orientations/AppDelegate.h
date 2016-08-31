d20e40efd33687984e91b5799e226f148d9755f3
Martin Perry <martinperry@gatech.edu>
2016-08-31 16:52:24 -0400

    Simple Rotation Label

---
+++ orientations/orientations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  orientations
+//
+//  Created by admin on 8/31/16.
+//  Copyright © 2016 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ orientations/orientations/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  orientations
+//
+//  Created by admin on 8/31/16.
+//  Copyright © 2016 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
