1a8b147a484be58bf9d21f41065a5aca6aa073b3
Martin Perry <martinperry@gatech.edu>
2016-09-02 15:32:59 -0400

    Restructure app
    added app to show different view containers based on orientation

---
+++ Restructure/Restructure/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Restructure
+//
+//  Created by admin on 9/2/16.
+//  Copyright © 2016 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
